5c61774ad3d3b0271320339a47386df290931f22
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-01-18 11:29:58 +0400

    FS

--- modules/billboard-fragment-collection-contributor/src/main/java/com/liferay/fragment/collection/contributor/billboard/BillboardFragmentCollectionContributor.java
+++ modules/billboard-fragment-collection-contributor/src/main/java/com/liferay/fragment/collection/contributor/billboard/BillboardFragmentCollectionContributor.java
@@ -1,3 +1,17 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
 package com.liferay.fragment.collection.contributor.billboard;
 
 import com.liferay.fragment.contributor.BaseFragmentCollectionContributor;
@@ -12,7 +26,8 @@ import org.osgi.service.component.annotations.Reference;
  * @author evanthibodeau
  */
 @Component(service = FragmentCollectionContributor.class)
-public class BillboardFragmentCollectionContributor extends BaseFragmentCollectionContributor {
+public class BillboardFragmentCollectionContributor
+	extends BaseFragmentCollectionContributor {
 
 	@Override
 	public String getFragmentCollectionKey() {
@@ -27,8 +42,6 @@ public class BillboardFragmentCollectionContributor extends BaseFragmentCollecti
 	@Reference(
 		target = "(osgi.web.symbolicname=com.liferay.fragment.collection.contributor.billboard)"
 	)
-
-
 	private ServletContext _servletContext;
 
 }
