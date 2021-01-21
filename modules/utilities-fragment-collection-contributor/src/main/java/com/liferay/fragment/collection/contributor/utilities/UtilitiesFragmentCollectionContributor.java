add25fac0b5f4df025814c5afb6911c53bf60c70
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-01-21 12:10:25 +0400

    Add Utilities Collection

---
+++ modules/utilities-fragment-collection-contributor/src/main/java/com/liferay/fragment/collection/contributor/utilities/UtilitiesFragmentCollectionContributor.java
@@ -0,0 +1,50 @@
+/**
+ * Copyright (c) 2000-present Liferay, Inc. All rights reserved.
+ *
+ * This library is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU Lesser General Public License as published by the Free
+ * Software Foundation; either version 2.1 of the License, or (at your option)
+ * any later version.
+ *
+ * This library is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU Lesser General Public License for more
+ * details.
+ */
+
+package com.liferay.fragment.collection.contributor.utilities;
+
+import com.liferay.fragment.contributor.BaseFragmentCollectionContributor;
+import com.liferay.fragment.contributor.FragmentCollectionContributor;
+
+import javax.servlet.ServletContext;
+
+import org.osgi.service.component.annotations.Component;
+import org.osgi.service.component.annotations.Reference;
+
+/**
+ * @author evanthibodeau
+ */
+@Component(
+	property = "fragment.collection.key=UTILITIES_FRAGMENTS",
+	service = FragmentCollectionContributor.class
+)
+public class UtilitiesFragmentCollectionContributor
+	extends BaseFragmentCollectionContributor {
+
+	@Override
+	public String getFragmentCollectionKey() {
+		return "UTILITIES_FRAGMENTS";
+	}
+
+	@Override
+	public ServletContext getServletContext() {
+		return _servletContext;
+	}
+
+	@Reference(
+		target = "(osgi.web.symbolicname=com.liferay.fragment.collection.contributor.utilities)"
+	)
+	private ServletContext _servletContext;
+
+}
