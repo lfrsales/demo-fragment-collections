fa328e1beab90b66d0628bf38922bb14d1648cc1
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-02-03 20:32:50 +0400

    Add generation of description when included in collection.json or fragment.json.

--- generateFragmentDocs.py
+++ generateFragmentDocs.py
@@ -1,47 +1,66 @@
 import glob, json, os
 
-def write_field(file, field):
+def write_field(file, langDict, field):
 	file.write("\n")
-	file.write(field['label'])
-	file.write(" | " + field['type'])
+	file.write(langDict[field["label"]])
+	file.write(" | " + field["type"])
 
 	if "defaultValue" in field:
-		file.write(" | " + str(field['defaultValue']))
+		file.write(" | " + str(field["defaultValue"]))
 
-collectionsReadmeFile = open('collections.md', 'w')
+collectionsReadmeFile = open("collections.md", "w")
 
 for module in glob.glob("./modules/*-fragment-collection-contributor"):
+	langDict = {}
+
+	langFile = open(module + "/src/main/resources/content/Language.properties")
+
+	langFileLines = langFile.readlines()
+
+	for line in langFileLines:
+		keyValuePair = line.split("=")
+
+		langDict[keyValuePair[0]] = keyValuePair[1].rstrip()
 
 	for collection in glob.glob(module + "/src/main/resources/**/dependencies/collection.json", recursive=True):
 		openCollection = open(collection)
+
 		collectionData = json.load(openCollection)
 
-		collectionsReadmeFile.write('<h3>' + collectionData['name'] + '</h3>\n\n')
-	
+		collectionsReadmeFile.write("<h2>" + langDict[collectionData["name"]] + "</h2>\n\n")
+
+		if "description" in collectionData:
+			collectionsReadmeFile.write(collectionData["description"] + "\n\n")
+
 	for fragment in glob.glob(module + "/src/main/resources/**/dependencies/*[!.json]", recursive=True):
-		fragmentJSON = open(fragment + '/fragment.json')
+		fragmentJSON = open(fragment + "/fragment.json")
 
 		fragmentJSONData = json.load(fragmentJSON)
 
-		collectionsReadmeFile.write('<h4>' + fragmentJSONData['name'] + '</h4>\n\n')
+		fragmentName = langDict[fragmentJSONData["name"]]
 
-		thumbnailPath = module + "/src/main/resources/META-INF/resources/thumbnails/" + fragmentJSONData['thumbnail']
+		collectionsReadmeFile.write("<h3>" + fragmentName + "</h3>\n\n")
 
-		collectionsReadmeFile.write("![Image of " + fragmentJSONData['name'] + "](" + thumbnailPath + ")\n\n")
+		if "description" in fragmentJSONData:
+			collectionsReadmeFile.write(fragmentJSONData["description"] + "\n\n")
 
-		if os.stat(fragment + '/index.json').st_size != 0:
-			indexJSON = open(fragment + '/index.json')
+		thumbnailPath = module + "/src/main/resources/META-INF/resources/thumbnails/" + fragmentJSONData["thumbnail"]
+
+		collectionsReadmeFile.write("![Image of " + fragmentName + "](" + thumbnailPath + ")\n\n")
+
+		if os.stat(fragment + "/index.json").st_size != 0:
+			indexJSON = open(fragment + "/index.json")
 
 			indexJSONData = json.load(indexJSON)
 
 			if "fieldSets" in indexJSONData:
-				collectionsReadmeFile.write("Configuration | type | Default Value\n")
-				collectionsReadmeFile.write("------------- | ---- | -------------")
+				collectionsReadmeFile.write("Configuration | Type | Default Value\n")
+				collectionsReadmeFile.write("------------- | ---- | ---------------")
 
-				for fieldSet in indexJSONData['fieldSets']:
+				for fieldSet in indexJSONData["fieldSets"]:
 					if "fields" in fieldSet:
-						for field in fieldSet['fields']:
-							write_field(collectionsReadmeFile, field)
+						for field in fieldSet["fields"]:
+							write_field(collectionsReadmeFile, langDict, field)
 
 				collectionsReadmeFile.write("\n\n")
 
