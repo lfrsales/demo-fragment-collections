19a626e14f5ba27b02af4167ca0dce35902fbb2a
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-02-28 09:50:13 +0400

    Update release notes.

--- release.sh
+++ release.sh
@@ -1,10 +1,12 @@
 hub release create \
-	-a modules/billboard-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.billboard-1.0.0.jar \
-	-a modules/billboard-theme-contributor/build/libs/billboard.theme.contributor-1.0.0.jar \
-	-a modules/d3-theme-contributor/build/libs/d3.theme.contributor-1.0.0.jar \
-	-a modules/formed-components-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.formed.components-1.0.0.jar \
-	-a modules/formed-content-display-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.formed.content.display-1.0.0.jar \
-	-a modules/header-components-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.header.components-1.0.0.jar \
-	-a modules/integrations-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.integrations-1.0.0.jar \
-	-a modules/utilities-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.utilities-1.0.0.jar \
-	-m 'v0.0.2' 'v0.0.2'
+	--attach modules/billboard-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.billboard-1.0.0.jar \
+	--attach modules/billboard-theme-contributor/build/libs/billboard.theme.contributor-1.0.0.jar \
+	--attach modules/d3-theme-contributor/build/libs/d3.theme.contributor-1.0.0.jar \
+	--attach modules/formed-components-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.formed.components-1.0.0.jar \
+	--attach modules/formed-content-display-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.formed.content.display-1.0.0.jar \
+	--attach modules/header-components-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.header.components-1.0.0.jar \
+	--attach modules/integrations-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.integrations-1.0.0.jar \
+	--attach modules/utilities-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.utilities-1.0.0.jar \
+	--message 'v0.0.3' \
+	--message 'Include updates to Tableau fragment.' \
+	'v0.0.3'
