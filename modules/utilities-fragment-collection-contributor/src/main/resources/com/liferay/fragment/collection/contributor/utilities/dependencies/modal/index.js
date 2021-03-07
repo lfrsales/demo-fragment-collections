978f9b887f6e3ef1e81ce2ecdf2878745c84d54c
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-03-07 13:33:45 +0400

    Rename container class to match use case.

--- modules/utilities-fragment-collection-contributor/src/main/resources/com/liferay/fragment/collection/contributor/utilities/dependencies/modal/index.js
+++ modules/utilities-fragment-collection-contributor/src/main/resources/com/liferay/fragment/collection/contributor/utilities/dependencies/modal/index.js
@@ -1,40 +1,40 @@
 const editMode = document.body.classList.contains('has-edit-mode-menu');
 
-const offerModal = fragmentElement.querySelector('.modal-root');
+const fragmentModal = fragmentElement.querySelector('.modal-root');
 
 const backdrop = document.createElement('div');
 
 const delay = configuration.delay;
 
 const controller = new AbortController();
+
 const signal = controller.signal;
 
 const closeModal = () => {
 	document.body.removeChild(backdrop);
 
-	offerModal.classList.remove('show');
+	fragmentModal.classList.remove('show');
 
-	setTimeout(() => offerModal.style.display = 'none', 500);
+	setTimeout(() => fragmentModal.style.display = 'none', 500);
 
 	controller.abort();
 }
 
 if (editMode) {
-    offerModal.style.display = 'block';
-    offerModal.classList.remove('fade', 'modal');
+    fragmentModal.style.display = 'block';
+    fragmentModal.classList.remove('fade', 'modal');
 }
 else {	
 	setTimeout(
 		function() {
-    		offerModal.style.display = 'block';
-			offerModal.classList.add('show');
+    		fragmentModal.style.display = 'block';
+			fragmentModal.classList.add('show');
 
 			backdrop.className = 'modal-backdrop fade show';
 
 			document.body.appendChild(backdrop);
 
-			// Add listeners to close modal
-			offerModal.querySelectorAll('[data-dismiss="modal"]').forEach(item => {
+			fragmentModal.querySelectorAll('[data-dismiss="modal"]').forEach(item => {
 				item.addEventListener(
 					'click',
 					event => {
