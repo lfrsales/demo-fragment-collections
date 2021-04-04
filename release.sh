5051243c6bc283c9340e3ff4bce67c3c6092cb8b
Evan Thibodeau <evan.thibodeau@liferay.com>
2021-04-04 12:32:06 +0400

    Update release v0.0.7

--- release.sh
+++ release.sh
@@ -7,6 +7,6 @@ hub release create \
 	--attach modules/header-components-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.header.components-1.0.0.jar \
 	--attach modules/integrations-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.integrations-1.0.0.jar \
 	--attach modules/utilities-fragment-collection-contributor/build/libs/com.liferay.fragment.collection.contributor.utilities-1.0.0.jar \
-	--message 'v0.0.6' \
-	--message 'Add Overlay Banner and Mappable Video Fragments.' \
-	'v0.0.6'
+	--message 'v0.0.7' \
+	--message 'Fix mappable video when using a non mapped video.' \
+	'v0.0.7'
