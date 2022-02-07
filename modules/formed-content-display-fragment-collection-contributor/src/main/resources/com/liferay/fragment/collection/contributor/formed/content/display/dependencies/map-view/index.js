9713ce69cf28c3faf75ca4529f9d849d3c2690d4
Evan Thibodeau <evan.thibodeau@liferay.com>
2022-02-07 13:22:32 +0400

    Fix Mapview fragment for display pages on 7.4.

--- modules/formed-content-display-fragment-collection-contributor/src/main/resources/com/liferay/fragment/collection/contributor/formed/content/display/dependencies/map-view/index.js
+++ modules/formed-content-display-fragment-collection-contributor/src/main/resources/com/liferay/fragment/collection/contributor/formed/content/display/dependencies/map-view/index.js
@@ -4,7 +4,7 @@ if (document.body.classList.contains('has-edit-mode-menu')) {
 	infoDiv.classList.add("alert");
 	infoDiv.classList.add("alert-info");
 
-	const newContent = document.createTextNode("Add this fragment to a display page or collection display fragment to show a map for geolocation content.");
+	const newContent = document.createTextNode("Add this fragment to a display page to show a map for geolocation content.");
 
 	infoDiv.appendChild(newContent);
 
